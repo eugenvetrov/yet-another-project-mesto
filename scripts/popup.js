78e7450d041e0fcbc8f226b2b718609e2e02d0ff
eugenvetrov <evg.vetrow@yandex.ru>
2021-12-29 00:55:17 +0500

    Внести исправления

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,23 +1,34 @@
 const editButton = document.querySelector(".profile__info-edit");
 const popup = document.querySelector(".popup");
-const profileEditSubmit = document.querySelector(".popup__submit");
-let editProfileName = document.querySelector(".popup__name");
-let editProfileDescription = document.querySelector(".popup__description");
+const profileEditSubmit = document.querySelector(".popup__form");
+let editProfileName = document.querySelector(".popup__text:nth-of-type(1)");
+let editProfileDescription = document.querySelector(".popup__text:nth-of-type(2)");
+/* Выполнил поиск по сложным селекторам, так как в ревью отмечено, 
+что класс должен быть одинаковым. Если необходимо
+создать модификаторы для полей в классах, готов переделать */
 let profileName = document.querySelector(".profile__info-name");
 let profileDescription = document.querySelector(".profile__info-description");
 const popupClose = document.querySelector(".popup__close-icon")
-editButton.addEventListener("click", () => {
+
+const openPopup = () => {
 	popup.classList.add("popup_opened");
-});
-profileEditSubmit.addEventListener("click", (event) => {
-	event.preventDefault();
-    profileName.textContent = editProfileName.value;
-	profileName.title = editProfileName.value;
-	profileDescription.textContent = editProfileDescription.value;
-	profileDescription.title = editProfileDescription.value;
+	editProfileName.value = profileName.textContent;
+	editProfileDescription.value = profileDescription.textContent;
+}
+
+const closePopup = () => {
 	popup.classList.remove("popup_opened");
-});
-popupClose.addEventListener("click", (event) => {
+}
+
+const editProfilePopup = (event) => {
 	event.preventDefault();
-	popup.classList.remove("popup_opened");
-})
+    profileName.textContent = editProfileName.value
+	profileDescription.textContent = editProfileDescription.value
+	closePopup();
+}
+
+editButton.addEventListener("click", openPopup);
+
+profileEditSubmit.addEventListener("submit", editProfilePopup);
+
+popupClose.addEventListener("click", closePopup)
